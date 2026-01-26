7c2f8e668ec614c50e6044e4499f55f4f852e83f
nisha-codes775 <nishasinghrathore858@gmail.com>
2026-01-26 10:20:17 +0530

    subarray equals k

---
+++ prefixsum/subarray_equals_k.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int subarraySum(vector<int>& nums, int k) {
+        int n=nums.size();
+        int count=0;
+        int sum=0;
+        int totalsum=0;
+        vector<int>prefixsum(n);
+        prefixsum[0]=nums[0];
+        for(int i=1;i<n;i++){
+            prefixsum[i]=prefixsum[i-1]+nums[i];
+        }
+        unordered_map<int,int>m;
+        for(int j=0;j<n;j++){
+            if(prefixsum[j]==k){
+                count++;
+            }
+            int val=prefixsum[j]-k;
+            if(m.find(val)!=m.end()){
+                count+=m[val];
+            }
+            if(m.find(prefixsum[j])==m.end()){
+                m[prefixsum[j]]=0;
+            }
+            m[prefixsum[j]]++;
+        }
+        return count;
+    }
+};
